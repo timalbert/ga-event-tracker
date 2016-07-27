eb37691874300cf0061e015090bdfd0b5d6cccfd
Tim Albert <timothy.m.albert@gmail.com>
2016-07-27 10:32:03 -0400

    initial commit

--- main.js
+++ main.js
@@ -5,10 +5,15 @@
 
 (function ($) {
 
+<<<<<<< f8d8d335c1ce1b3c7a84f4093e4d895e81df9d35
   console.log('hemon');
 
   // data-ga-click="homepage hero, click: cta, item: More"
 
+=======
+  // data-ga-click="homepage hero, click: cta, item: More"
+  // data-ga-click="header, click: navigation, item: Logo"
+>>>>>>> initial commit
   $('[data-ga-click]').click(function(e) {
 
     var ga_event = $(this).data('ga-click'),
@@ -17,8 +22,11 @@
         ga_event_action = ga_event_split[1],
         ga_event_label = ga_event_split[2];
 
+<<<<<<< f8d8d335c1ce1b3c7a84f4093e4d895e81df9d35
     console.log(ga_event_category, ga_event_action, ga_event_label);
 
+=======
+>>>>>>> initial commit
     ga('send', {
       hitType: 'event',
       eventCategory: ga_event_category,
@@ -28,4 +36,8 @@
 
   });
 
+<<<<<<< f8d8d335c1ce1b3c7a84f4093e4d895e81df9d35
 })(jQuery);
+=======
+})(jQuery);
+>>>>>>> initial commit
