4fafd48883be1d6dbc48a7166c81ef2a21a76111
Tim Albert <timothy.m.albert@gmail.com>
2016-07-27 10:37:31 -0400

    Clean up

--- main.js
+++ main.js
@@ -3,17 +3,12 @@
  * Contributing Author: Tim Albert
  */
 
+ // Example usage
+ // data-ga-click="homepage hero, click: cta, item: More"
+ // data-ga-click="header, click: navigation, item: Logo"
+
 (function ($) {
 
-<<<<<<< f8d8d335c1ce1b3c7a84f4093e4d895e81df9d35
-  console.log('hemon');
-
-  // data-ga-click="homepage hero, click: cta, item: More"
-
-=======
-  // data-ga-click="homepage hero, click: cta, item: More"
-  // data-ga-click="header, click: navigation, item: Logo"
->>>>>>> initial commit
   $('[data-ga-click]').click(function(e) {
 
     var ga_event = $(this).data('ga-click'),
@@ -22,11 +17,6 @@
         ga_event_action = ga_event_split[1],
         ga_event_label = ga_event_split[2];
 
-<<<<<<< f8d8d335c1ce1b3c7a84f4093e4d895e81df9d35
-    console.log(ga_event_category, ga_event_action, ga_event_label);
-
-=======
->>>>>>> initial commit
     ga('send', {
       hitType: 'event',
       eventCategory: ga_event_category,
@@ -36,8 +26,4 @@
 
   });
 
-<<<<<<< f8d8d335c1ce1b3c7a84f4093e4d895e81df9d35
 })(jQuery);
-=======
-})(jQuery);
->>>>>>> initial commit
